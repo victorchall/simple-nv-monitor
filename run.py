3fd18d9e07950501871bddd79901a4f729fbf714
Victor Hall <victor.charles.hall@gmail.com>
2022-09-14 21:51:16 -0400

    integrate nvsmi so I can expand

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-import nvsmi
+#file
+from nvsmi import nvsmi
 import time
 import os
 import sys
@@ -11,18 +12,6 @@ def clear():
         os.system("clear")
 
 def printProgressBar (currentValue, maxValue, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        currentValue - Required  : current value (Int)
-        maxValue     - Required  : max value (Int)
-        prefix       - Optional  : prefix string (Str)
-        suffix       - Optional  : suffix string (Str)
-        decimals     - Optional  : positive number of decimals in percent complete (Int)
-        length       - Optional  : character length of bar (Int)
-        fill         - Optional  : bar fill character (Str)
-        printEnd     - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
     percent = ("{0:." + str(decimals) + "f}").format(100 * (currentValue / float(maxValue)))
     filledLength = int(length * currentValue // maxValue)
     bar = fill * filledLength + '-' * (length - filledLength)
@@ -49,9 +38,10 @@ def printGpus():
     for gpu in gpus:
         vramGb = vramToGb(vramMb = gpu.mem_total)
         print(f"GPU ID: {gpu.id}, Name: {gpu.name}, VRAM: {vramGb} GB, Temp: {gpu.temperature}C")            
-        line2Prefix = f"     GPU Util:"
-        printProgressBar(currentValue = gpu.mem_used, maxValue = gpu.mem_total, prefix = "   GPU Memory:", suffix= "%", length=50, printEnd ="\n")
-        printProgressBar(currentValue = gpu.gpu_util, maxValue = 100, prefix = line2Prefix, suffix= "%", length=50, printEnd ="\n")
+        printProgressBar(currentValue = gpu.mem_used, maxValue = gpu.mem_total, prefix = "    Memory:", suffix= f"%   {gpu.mem_used} / {gpu.mem_total} MB", length=50, printEnd ="\n")
+        printProgressBar(currentValue = gpu.gpu_util, maxValue = 100, prefix = f"      Util:", suffix= f"%", length=50, printEnd ="\n")
+        printProgressBar(currentValue = gpu.power_draw, maxValue = gpu.enforced_power_limit, prefix = '     Power:', suffix= f"%   {gpu.power_draw:.1F} / {gpu.enforced_power_limit:.0F} W", length=50, printEnd ="\n")
+        #print(f'    Power: {gpu.power_draw:.1F} / {gpu.enforced_power_limit:.0F} W')
         print()  
 
 def main():
